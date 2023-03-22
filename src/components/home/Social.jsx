8802e253e5c3fbd03ff38e2b59dabbe079a3d50b
unknown <issang_b@etna-alternance.net>
2023-03-22 12:38:02 +0100

    portfolio

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -7,11 +7,11 @@ const Social = () => {
             <i class="uil uil-instagram"></i>
         </a>
 
-        <a href="https://www.dribbble.com/" className="home_social-icon" target="_blank">
-            <i class="uil uil-globe"></i>
+        <a href="https://github.com/Beviryon" className="home_social-icon" target="_blank">
+            <i class="uil uil-linkedin-alt"></i>
         </a>
 
-        <a href="https://www.github.com/" className="home_social-icon" target="_blank">
+        <a href="https://github.com/Beviryon" className="home_social-icon" target="_blank">
             <i class="uil uil-github-alt"></i>
         </a>
     </div>
