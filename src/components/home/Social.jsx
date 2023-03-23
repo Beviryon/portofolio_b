ed320ef34f5efd26d1a5733847e4c36ac2887ed9
unknown <issang_b@etna-alternance.net>
2023-03-23 23:06:56 +0100

    portfolio

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -7,7 +7,7 @@ const Social = () => {
             <i class="uil uil-instagram"></i>
         </a>
 
-        <a href="https://github.com/Beviryon" className="home_social-icon" target="_blank">
+        <a href="https://www.linkedin.com/in/beviryon-issanga-ngoulou-82061b238/" className="home_social-icon" target="_blank">
             <i class="uil uil-linkedin-alt"></i>
         </a>
 
