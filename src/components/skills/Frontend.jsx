8685278020d3789ce456c874a1b2b742658259c0
Beviryon ISSANGA-NGOULOU <beviryon.issanga-ngoulou@eurexo-ced.fr>
2024-07-10 18:01:48 +0200

    new page skill

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -1,90 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHtml5, faCss3Alt, faJsSquare, faBootstrap, faReact } from '@fortawesome/free-brands-svg-icons';
 
 const Frontend = () => {
+  const frontendSkills = [
+    { name: 'HTML', icon: faHtml5, level: 90 },
+    { name: 'CSS', icon: faCss3Alt, level: 85 },
+    { name: 'JavaScript', icon: faJsSquare, level: 80 },
+    { name: 'Bootstrap', icon: faBootstrap, level: 75 },
+    { name: 'React.js', icon: faReact, level: 70 },
+  ];
+
   return (
-    <div className="skills_content">
-        <h3 className="skills_title"> Frontend developer</h3>
-
-        <div className="skills_box">
-            <div className="skills_group">
-                <div className="skills_data">
-
-                    <i class='bx bx-badge-check'></i>
-
-                    <div>
-                        <h3 className="skills_name">HTML</h3>
-                        <span className="skills_leve">..................</span>
-                    </div>
-                </div>
-
-                <div className="skills_data">
-                <i class='bx bx-badge-check'></i>
-
-                <div>
-                    <h3 className="skills_name">CSS</h3>
-                    <span className="skills_leve">..................</span>
-                </div>
-                </div>
-
-                <div className="skills_data">
-                <i class='bx bx-badge-check'></i>
-
-                <div>
-                    <h3 className="skills_name">JavaScript</h3>
-                    <span className="skills_leve">..................</span>
-                </div>
-                </div>
+    <div className="skills_section">
+      <h3 className="skills_title">Frontend Developer</h3>
+      <div className="skills_container">
+        {frontendSkills.map((skill, index) => (
+          <div className="skills_card" key={index}>
+            <FontAwesomeIcon icon={skill.icon} className="skills_icon" />
+            <h3 className="skills_name">{skill.name}</h3>
+            <div className="skills_bar">
+              <div className="skills_progress" style={{ width: `${skill.level}%` }}></div>
             </div>
-
-            <div className="skills_group">
-                <div className="skills_data">
-                <i class='bx bx-badge-check'></i>
-
-                <div>
-                    <h3 className="skills_name">Bootstrap</h3>
-                    <span className="skills_leve">..................</span>
-                </div>
-                </div>
-
-                <div className="skills_data">
-                <i class='bx bx-badge-check'></i>
-
-                <div>
-                    <h3 className="skills_name">Typescript</h3>
-                    <span className="skills_leve">..................</span>
-                </div>
-                </div>
-
-                <div className="skills_data">
-                <i class='bx bx-badge-check'></i>
-
-                <div>
-                    <h3 className="skills_name">Next.Js</h3>
-                    <span className="skills_leve">..................</span>
-                </div>
-                </div>
-
-                <div className="skills_data">
-                <i class='bx bx-badge-check'></i>
-
-                <div>
-                    <h3 className="skills_name">React.Js</h3>
-                    <span className="skills_leve">..................</span>
-                </div>
-                </div>
-
-                <div className="skills_data">
-                <i class='bx bx-badge-check'></i>
-
-                <div>
-                    <h3 className="skills_name">React-native</h3>
-                    <span className="skills_leve">..................</span>
-                </div>
-                </div>
-            </div>
-        </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Frontend
+export default Frontend;
