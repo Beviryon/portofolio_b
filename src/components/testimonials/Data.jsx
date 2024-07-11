c21440e6fdba9b51b6c187a56ac6b79f56de67ca
Beviryon ISSANGA-NGOULOU <beviryon.issanga-ngoulou@eurexo-ced.fr>
2024-07-11 13:09:09 +0200

    modification

--- src/components/testimonials/Data.jsx
+++ src/components/testimonials/Data.jsx
@@ -1,41 +1,85 @@
 import Image1 from "../../assets/testimonial1.png";
 import Image2 from "../../assets/testimonial2.png";
 import Image3 from "../../assets/testimonial3.png";
+import Image4 from "../../assets/projet1.jpg";
+import Image5 from "../../assets/projet2.jpg";
+import Image6 from "../../assets/projet3.jpg";
+import Image7 from "../../assets/projet4.jpg";
+import Image8 from "../../assets/projet5.jpg";
+import Image9 from "../../assets/projet6.jpg";
+import Image10 from "../../assets/projet7.jpg";
+
 
 export const Data = [
   {
     id: 1,
-    image: Image1,
-    title: "Blog Mentor Goal",
+    image: Image4,
+    title: "Site Ecommerce de Deslo'group",
     //description: "Création d’un blog pour Mentor Goal afin de partager des ressources et articles dédiées à la recherche d’emploi en utilisant Strapi.io pour le côté Back/CMS et React.JS pour du Front-end.",
     github: "https://github.com/mentor-goal/blog"
   },
   {
     id: 2,
-    image: Image2,
-    title: "My Website",
+    image: Image5,
+    title: "site Workshop",
     //description: "Développement d’un site e-commerce. Connexion avec l’API, utilisation du Next.Js pour le côté Front-end.",
     github: "https://github.com/mywebsite"
   },
   {
     id: 3,
-    image: Image3,
-    title: "My API",
+    image: Image9,
+    title: "Site Restauration",
     //description: "Développement côté back-end d’une API. Installation des outils, mise en place d’une base de données et séquençage, utilisation de express et d’un système de routage, Mise en œuvre du CRUD, Tokens privés d’authentification, mise en place d’un Middleware.",
     github: "https://github.com/myapi"
   },
   {
     id: 4,
-    image: Image3,
-    title: "My API",
+    image: Image7,
+    title: "Site de la association UL",
     //description: "Développement côté back-end d’une API. Installation des outils, mise en place d’une base de données et séquençage, utilisation de express et d’un système de routage, Mise en œuvre du CRUD, Tokens privés d’authentification, mise en place d’un Middleware.",
     github: "https://github.com/myapi"
   },
   {
     id: 5,
-    image: Image3,
-    title: "My API",
+    image: Image8,
+    title: "Projet d'une application intra pour une école",
     //description: "Développement côté back-end d’une API. Installation des outils, mise en place d’une base de données et séquençage, utilisation de express et d’un système de routage, Mise en œuvre du CRUD, Tokens privés d’authentification, mise en place d’un Middleware.",
     github: "https://github.com/myapi"
   },
+  {
+    id: 6,
+    image: Image6,
+    title: "Interface de connexion pour l'association UL",
+    //description: "Développement côté back-end d’une API. Installation des outils, mise en place d’une base de données et séquençage, utilisation de express et d’un système de routage, Mise en œuvre du CRUD, Tokens privés d’authentification, mise en place d’un Middleware.",
+    github: "https://github.com/myapi"
+  },
+  {
+    id: 7,
+    image: Image10,
+    title: "Gestion de Panier pour un site e-commerce",
+    //description: "Développement côté back-end d’une API. Installation des outils, mise en place d’une base de données et séquençage, utilisation de express et d’un système de routage, Mise en œuvre du CRUD, Tokens privés d’authentification, mise en place d’un Middleware.",
+    github: "https://github.com/myapi"
+  },
+  {
+    id: 8,
+    image: Image1,
+    title: "Site Workshop ETNA",
+    //description: "Développement côté back-end d’une API. Installation des outils, mise en place d’une base de données et séquençage, utilisation de express et d’un système de routage, Mise en œuvre du CRUD, Tokens privés d’authentification, mise en place d’un Middleware.",
+    github: "https://github.com/myapi"
+  },
+  {
+    id: 9,
+    image: Image2,
+    title: "Site E-commerce",
+    //description: "Développement côté back-end d’une API. Installation des outils, mise en place d’une base de données et séquençage, utilisation de express et d’un système de routage, Mise en œuvre du CRUD, Tokens privés d’authentification, mise en place d’un Middleware.",
+    github: "https://github.com/myapi"
+  },
+  {
+    id: 10,
+    image: Image3,
+    title: "API d'un site E-commerce",
+    //description: "Développement côté back-end d’une API. Installation des outils, mise en place d’une base de données et séquençage, utilisation de express et d’un système de routage, Mise en œuvre du CRUD, Tokens privés d’authentification, mise en place d’un Middleware.",
+    github: "https://github.com/myapi"
+  },
+
 ];
