90b524099c4361585e93a3771c1cc40f1143a937
Beviryon ISSANGA-NGOULOU <beviryon.issanga-ngoulou@eurexo-ced.fr>
2024-07-11 12:16:54 +0200

    modification

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./about.css";
 import AboutImg from "../../assets/about.jpg";
-import CV from "../../assets/Beviryon issanga ngoulou cv alternance.pdf"
+import CV from "../../assets/bEVIRYON ISSANGA NGOULOU.pdf"
 import Info from './Info';
 
 const About = () => {
