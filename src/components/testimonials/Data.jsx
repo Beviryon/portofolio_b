cf8813babe2fcc00ebd81b5b0309254a565837dc
Beviryon ISSANGA-NGOULOU <beviryon.issanga-ngoulou@eurexo-ced.fr>
2024-07-11 13:21:58 +0200

    modification

--- src/components/testimonials/Data.jsx
+++ src/components/testimonials/Data.jsx
@@ -15,70 +15,70 @@ export const Data = [
     id: 1,
     image: Image4,
     title: "Site Ecommerce de Deslo'group",
-    //description: "Création d’un blog pour Mentor Goal afin de partager des ressources et articles dédiées à la recherche d’emploi en utilisant Strapi.io pour le côté Back/CMS et React.JS pour du Front-end.",
-    github: "https://github.com/mentor-goal/blog"
+    description: "HTML-CSS-JS",
+    github: "https://github.com/Beviryon?tab=repositories"
   },
   {
     id: 2,
     image: Image5,
     title: "site Workshop",
-    //description: "Développement d’un site e-commerce. Connexion avec l’API, utilisation du Next.Js pour le côté Front-end.",
-    github: "https://github.com/mywebsite"
+    description: "ReactJs-NodeJs-MySQL",
+    github: "https://github.com/Beviryon?tab=repositories"
   },
   {
     id: 3,
     image: Image9,
     title: "Site Restauration",
-    //description: "Développement côté back-end d’une API. Installation des outils, mise en place d’une base de données et séquençage, utilisation de express et d’un système de routage, Mise en œuvre du CRUD, Tokens privés d’authentification, mise en place d’un Middleware.",
+    description: "HTML-CSS-JS",
     github: "https://github.com/Beviryon?tab=repositories"
   },
   {
     id: 4,
     image: Image7,
     title: "Site de la association UL",
-    //description: "Développement côté back-end d’une API. Installation des outils, mise en place d’une base de données et séquençage, utilisation de express et d’un système de routage, Mise en œuvre du CRUD, Tokens privés d’authentification, mise en place d’un Middleware.",
+    description: "HTML-CSS-JS",
     github: "https://github.com/Beviryon?tab=repositories"
   },
   {
     id: 5,
     image: Image8,
     title: "Projet d'une application intra pour une école",
-    //description: "Développement côté back-end d’une API. Installation des outils, mise en place d’une base de données et séquençage, utilisation de express et d’un système de routage, Mise en œuvre du CRUD, Tokens privés d’authentification, mise en place d’un Middleware.",
+    description: "ReactJs-NodeJs-MySQL",
     github: "https://github.com/Beviryon?tab=repositories"
   },
   {
     id: 6,
     image: Image6,
     title: "Interface de connexion pour l'association UL",
-    //description: "Développement côté back-end d’une API. Installation des outils, mise en place d’une base de données et séquençage, utilisation de express et d’un système de routage, Mise en œuvre du CRUD, Tokens privés d’authentification, mise en place d’un Middleware.",
+    description: "ReactJs-NodeJs-MySQL",
     github: "https://github.com/Beviryon?tab=repositories"
   },
   {
     id: 7,
     image: Image10,
     title: "Gestion de Panier pour un site e-commerce",
-    //description: "Développement côté back-end d’une API. Installation des outils, mise en place d’une base de données et séquençage, utilisation de express et d’un système de routage, Mise en œuvre du CRUD, Tokens privés d’authentification, mise en place d’un Middleware.",
+    description: "HTML-CSS-JS",
     github: "https://github.com/Beviryon?tab=repositories"
   },
   {
     id: 8,
     image: Image1,
-    title: "Site Workshop ETNA",
-    //description: "Développement côté back-end d’une API. Installation des outils, mise en place d’une base de données et séquençage, utilisation de express et d’un système de routage, Mise en œuvre du CRUD, Tokens privés d’authentification, mise en place d’un Middleware.",
+    title: "Site Workshop ETNA Mentor Goal",
+    description: "Next-TypeScript",
     github: "https://github.com/Beviryon?tab=repositories"
   },
   {
     id: 9,
     image: Image2,
     title: "Site E-commerce",
-    //description: "Développement côté back-end d’une API. Installation des outils, mise en place d’une base de données et séquençage, utilisation de express et d’un système de routage, Mise en œuvre du CRUD, Tokens privés d’authentification, mise en place d’un Middleware.",
+    description: "Next-TypeScript",
     github: "https://github.com/Beviryon?tab=repositories"
   },
   {
     id: 10,
     image: Image3,
     title: "API d'un site E-commerce",
-    //description: "Développement côté back-end d’une API. Installation des outils, mise en place d’une base de données et séquençage, utilisation de express et d’un système de routage, Mise en œuvre du CRUD, Tokens privés d’authentification, mise en place d’un Middleware.",
+    description: "NodeJs-TypeScript",
     github: "https://github.com/Beviryon?tab=repositories"
   },
 
